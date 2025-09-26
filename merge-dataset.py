e1553f3d817506401d3718e760f8e0218d6bb144
Leonardo Tisato <10844472@polimi.it>
2025-09-26 13:19:50 +0200

    small fix

--- merge-dataset.py
+++ merge-dataset.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 base_filename = "data_css_challenge_"
 
-df_list = [pd.read_csv(f"data/{base_filename}_{i}.csv") for i in range(10)]
+df_list = [pd.read_csv(f"data/{base_filename}{i}.csv") for i in range(10)]
 merged_df = pd.concat(df_list, ignore_index=True)
 merged_df.to_csv("data/data_css_challenge.csv",index=False)
